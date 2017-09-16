6e2cadc8092504e31b477f6cf3c8b905ee9baf74
saltrichter <saltrichter@liveperson.com>
2017-09-16 05:38:39 +0200

    upgraded usage of api

--- src/dialogs/cookingIdeasDialog.js
+++ src/dialogs/cookingIdeasDialog.js
@@ -13,10 +13,19 @@ class cookingIdeasDialog {
         // Builder.Prompts.text(session, CookingIdeasDialogMessage.Cooking); // The result of this input will be forwarded to the next step
         // Builder.Message(session).addAttachment({contentType: "image/jpeg", contentUrl: 'http://www.seriouseats.com/recipes/assets_c/2015/07/20150728-homemade-whopper-food-lab-35-thumb-1500xauto-425129.jpg'})
         //     .setText(session, recipes.name+"\n"+recipes.ingredients+"\n"+recipes.description);
-        Builder.Prompts.
-            .attachment(session,)text(session, recipes.name+"\n")
-            .text(session,recipes.ingredients+"\n")
-            .text(session,recipes.description);
+        var card = new Builder.ThumbnailCard(session)
+            .title(recipes.name)
+            .subtitle(recipes.ingredients)
+            .text(recipes.description)
+            .images([
+                Builder.CardImage.create(session, 'http://www.seriouseats.com/recipes/assets_c/2015/07/20150728-homemade-whopper-food-lab-35-thumb-1500xauto-425129.jpg')
+            ])
+            .buttons([
+                Builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework', 'Get Started')
+            ]);
+        // attach the card to the reply message
+        var msg = new Builder.Message(session).addAttachment(card);
+        session.send(msg);
     }
 
 }
